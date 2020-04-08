398eb3837f3618c2bab1e3b54a313980ad08a1c6
sammysituma <samuelsituma@yahoo.com>
2020-04-08 21:05:54 +0300

    commit on master

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
-
+try {
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
+    useUnifiedTopology: true ,
 })
+console.log('MongoDB connected: ${connection.connection.host}');
+}
+ catch (error) {
+console.log('MongoDB error when connecting: ${error}');
+}
